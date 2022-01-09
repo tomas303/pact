27c88c1fe1a53be23b8b1df673f3ac7aebf51fc3
tomas303 <tomashorak@post.cz>
2022-01-09 19:27:41 +0100

    overhaul grid

--- uapp.pas
+++ uapp.pas
@@ -10,7 +10,7 @@ uses
   rea_idesigncomponent, rea_udesigncomponent,
   trl_ilog, trl_idifactory, trl_iExecutor, trl_dicontainer, trl_ilauncher,
   trl_iprops, trl_imetaelement, rea_irenderer,
-  Forms;
+  Forms, Graphics;
 
 type
 
@@ -23,12 +23,16 @@ type
     fRenderer: IRenderer;
   private
     fMainFormData: TFormData;
+    fNamesGridData: TGridData2;
+    fTestEditData: TEditData;
     fGUI: IDesignComponentApp;
   private
     function NewProps: IProps;
     function NewAction(AActionID: integer): IFluxAction;
     function NewNotifier(const AActionID: integer): IFluxNotifier;
     function NewMainForm: IDesignComponentForm;
+    function NewNamesGrid:  IDesignComponentGrid;
+    function NewTestEdit:  IDesignComponentEdit;
   protected
     procedure RegisterAppServices; override;
     procedure BeforeLaunch; override;
@@ -76,6 +80,44 @@ begin
   fFluxFuncReg.RegisterFunc(uappfunc.TMoveFunc.Create(-102, fMainFormData));
 end;
 
+function TApp.NewNamesGrid: IDesignComponentGrid;
+begin
+  Result :=
+   IDesignComponentGrid(DIC.Locate(IDesignComponentGrid, '',
+    NewProps
+      .SetObject('Data', fNamesGridData)
+      .SetIntf('EdTextChangedNotifier', NewNotifier(-150))
+      .SetIntf('EdKeyDownNotifier', NewNotifier(-151))
+      .SetInt('MMHeight', 1000)
+      .SetInt('MMWidth', 1000)
+      .SetInt(cProps.RowMMHeight, 25)
+      .SetInt(cProps.ColMMWidth, 25)
+      .SetInt(cProps.ColOddColor, clLime)
+      .SetInt(cProps.ColEvenColor, clAqua)
+      .SetInt(cProps.RowOddColor, clRed)
+      .SetInt(cProps.RowEvenColor, clYellow)
+      .SetInt(cProps.Color, clMaroon)
+      .SetInt('LaticeColColor', clRed)
+      .SetInt('LaticeColSize', 10)
+      .SetInt('LaticeRowColor', clGreen)
+      .SetInt('LaticeRowSize', 2)
+  ));
+  fFluxFuncReg.RegisterFunc(uappfunc.TGridEdTextChangedFunc.Create(-150, fNamesGridData, NewNotifier(-400)));
+  fFluxFuncReg.RegisterFunc(uappfunc.TGridEdKeyDownFunc.Create(-151, fNamesGridData, NewNotifier(-400)));
+end;
+
+function TApp.NewTestEdit: IDesignComponentEdit;
+begin
+  Result :=
+   IDesignComponentEdit(DIC.Locate(IDesignComponentEdit, '',
+    NewProps
+      .SetObject('Data', fTestEditData)
+      .SetBool('Flat', True)
+      .SetIntf('TextChangedNotifier', NewNotifier(-160))
+   ));
+  fFluxFuncReg.RegisterFunc(uappfunc.TextChangedFunc.Create(-160, fTestEditData));
+end;
+
 procedure TApp.RegisterAppServices;
 var
   mReg: TDIReg;
@@ -91,6 +133,8 @@ begin
   //RegReact.RegisterDesignComponent(TDesignComponentApp, IDesignComponentApp);
 
   RegReact.RegisterDesignComponent(TDesignComponentForm2, IDesignComponentForm);
+  RegReact.RegisterDesignComponent(TDesignComponentGrid2, IDesignComponentGrid);
+  RegReact.RegisterDesignComponent(TDesignComponentEdit2, IDesignComponentEdit);
 end;
 
 procedure TApp.BeforeLaunch;
@@ -106,8 +150,21 @@ begin
   fRenderer := IRenderer(DIC.Locate(IRenderer));
 
   fMainFormData := TFormData.Create;
+  fMainFormData.Left := 0;
+  fMainFormData.Top := 0;
+  fMainFormData.Width := 800;
+  fMainFormData.Height := 400;
 
-  fGUI := TGUI.Create(NewMainForm);
+  fNamesGridData := TGridData2.Create(TDummyGridDataProvider.Create);
+  fNamesGridData.RowCount := 10;
+  fNamesGridData.ColCount := 2;
+  fNamesGridData.ReadData;
+
+  fTestEditData := TEditData.Create;
+  fTestEditData.Focused := True;
+  fTestEditData.Text := 'testovaci edit';
+
+  fGUI := TGUI.Create(NewMainForm, NewNamesGrid);
 
   fFluxFuncReg.RegisterFunc(uappfunc.TRenderGUIFunc.Create(-400, fGUI, fRenderer));
   NewNotifier(-400).Notify;
